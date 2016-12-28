d4121baba63f9a1c78493ae76453b32984867497
ZZZZcccc <zczhangcui@foxmail.com>
2016-12-28 20:28:08 +0800

    modify style

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,53 @@
 $(function () {
-	$("nav li").hover(function() {
-		$(this).addClass('active');
-	}, function() {
-		$(this).removeClass('active');
-	});
-
 	new WOW().init();
 
-	$(".btn").hover(function() {
-		$(this).addClass('animated pulse');
-	}, function() {
-		$(this).removeClass('animated pulse')
+	var navtimer=null;
+	//导航栏点击事件
+	$(".nav").on('click', 'a', function(event) {
+		var that=$(this);
+		var txt=$(event.target).attr('href')+"container";
+		$("html,body").animate({scrollTop:$(txt).offset().top}, 400);
+		return false;
 	});
 
-	// $(".thumbnail a").hover(function() {
-	// 	$(this).parent(".thumbnail").addClass('animated pulse');
-	// }, function() {
-	// 	$(this).parent(".thumbnail").removeClass('animated pulse');
-	// });
-})
+	//屏幕滚动事件
+	$(window).scroll(function() {
+		if($("#aboutcontainer").offset().top-$(window).scrollTop()<=0){
+			$(".navbar").addClass('changebg');
+		}else{
+			$(".navbar").removeClass('changebg');
+		}
+		if($("#homecontainer").offset().top-$(window).scrollTop()<=0.5*$(window).height()){
+			clearTimeout(navtimer);
+			navtimer=setTimeout(function(){
+				$(".nav").find('a').removeClass('active');
+				$(".nav li").eq(0).find('a').addClass('active');
+			},200);
+		}
+		if($("#aboutcontainer").offset().top-$(window).scrollTop()<=0.5*$(window).height()){
+			clearTimeout(navtimer);
+			navtimer=setTimeout(function(){
+				$(".nav").find('a').removeClass('active');
+				$(".nav li").eq(1).find('a').addClass('active');
+			},200);
+		}	
+		if($("#portfoliocontainer").offset().top-$(window).scrollTop()<=0.5*$(window).height()){
+			clearTimeout(navtimer);
+			navtimer=setTimeout(function(){
+				$(".nav").find('a').removeClass('active');
+				$(".nav li").eq(2).find('a').addClass('active');
+			},200);
+		}
+		if($("#contactcontainer").offset().top-$(window).scrollTop()<=0.5*$(window).height()){
+			clearTimeout(navtimer);
+			navtimer=setTimeout(function(){
+				$(".nav").find('a').removeClass('active');
+				$(".nav li").eq(3).find('a').addClass('active');
+			},200);
+		}
+	});
+
+	$(window).trigger('scroll');
+
+});
 
