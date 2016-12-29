d005484861a25438b8f190717f358858a23e6e07
ZZZZcccc <zczhangcui@foxmail.com>
2016-12-29 11:20:10 +0800

    modify loading layer

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,4 @@
 $(function () {
-	function loading(){
-		$(".loading h1").animate({fontSize:'4em'}, 2500 ,function(){
-			$(this).animate({fontSize:'3em'}, 2500);
-		});
-	}
-	loading();
-	var loadtimer=setInterval(function(){
-		loading();
-	},5200);
-
-	new WOW().init();
-
 	var navtimer=null;
 	//导航栏点击事件
 	$(".nav").on('click', 'a', function(event) {
@@ -62,10 +50,60 @@ $(function () {
 	//设置loading层的高度
 	$(".loading").css('height', $("body").height());
 
+	//**************以下为加载页面的相关设置
+	function drawFlame(color, px, py, radius) {
+	    this._.fillStyle = color;
+		this._.beginPath();
+		this._.arc(
+			px, py,
+			radius, 0,
+			Math.PI*2, false
+		);
+		this._.closePath();
+		this._.fill();
+	}
+	var loader={
+		width: 100,
+		height: 100,
+
+		stepsPerFrame: 4,
+		trailLength: 0.8,
+		pointDistance: 0.01,
+		fps: 20,
+
+		backgroundColor: '#000',
+
+		path: [
+			['arc', 60, 60, 30, 0, 360]
+		],
+
+		step: function(point, index, frame) {
+
+			var sizeMultiplier = 10;
+	        var radius = sizeMultiplier * (index > 0.5 ? 1-index : index);
+	        
+	        drawFlame.call(this, '#FF6C08', point.x*index, point.y, radius);
+	        drawFlame.call(this, '#FFD341', point.x, point.y*index, radius);
+	        drawFlame.call(this, '#FF3000', point.x, point.y, radius);
+
+		}
+	};
+
+	a=new Sonic(loader);
+	$("#canvascontainer").append(a.canvas);
+	a.play();
+	//**************以上为加载页面的相关设置
+
+	//点击加载页面的按钮后关闭加载页面
+	$(".loading .btn").click(closeloading);
 
 });
+
 //完全加载后取消loading层
-window.onload=function() {
+window.onload=closeloading;
+function closeloading(){
+	new WOW().init();
+	a.stop();
 	$(".loading").fadeOut(400);
-	clearInterval(loadtimer);
-};
+	document.getElementsByTagName('body')[0].style.overflow="visible";
+}
